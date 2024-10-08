b6704525a1428aecfccf77b35f2c5722f59ffa08
ahmed abdelaziz <a.abdelazizg@gmail.com>
2024-10-08 12:04:42 +0300

    fetch data for shipment

--- src/app/shipments/track/[trackingNumber]/page.jsx
+++ src/app/shipments/track/[trackingNumber]/page.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+"use client";
+
+import Address from "@/components/Address/Address";
+import ShippingHistoryTable from "@/components/ShippingHistoryTable/ShippingHistoryTable";
+import StepsProgressBar from "@/components/StepsProgressBar/StepsProgressBar";
+import { useEffect, useState } from "react"
+import { Col, Container, Row } from "react-bootstrap";
+import { toast } from "react-toastify";
 
 const TrackPage = ({ params }) => {
+  const [shipmentData, setShipmentData] = useState({});
+
+  const handleGetRequest = async (shipmentCode) => {
+    const BaseURL = "https://tracking.bosta.co"; // it must be in .env file and use {process.env.NEXT_BOSTA_BASEURL} for example
+
+    try {
+      const request = await fetch(`${BaseURL}/shipments/track/${shipmentCode}`, { method: "GET" }); // we can use axios
+      const data = await request.json();
+      console.log(data);
+      // if the code is not correct
+      if (data?.error && data?.status === "Not Found.") {
+        // we should navigate to home page or display error page for the user
+        toast(t.notfound);
+      }
+
+      // if the code is correct -----> we can use toolkit to access data from the components but i will use the props to pass the data
+      setShipmentData({...data});
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (params.trackingNumber) {
+      handleGetRequest(params.trackingNumber);
+    }
+  }, [params.trackingNumber]);
+
   return (
-    <div>Track: {params.trackingNumber}</div>
+    <section>
+      <Container>
+        <Row>
+          <Col className="mb-[20px] md:mb-0">
+            <StepsProgressBar
+              provider={shipmentData?.provider}
+              currentStatus={shipmentData?.CurrentStatus}
+              createDate={shipmentData?.CreateDate}
+              promisedDate={shipmentData?.PromisedDate}
+              trackingNumber={shipmentData?.TrackingNumber}
+            />
+          </Col>
+        </Row>
+
+        <Row className="flex-col md:flex-row">
+          <Col className="mb-[20px] md:mb-0">
+            <ShippingHistoryTable
+              transitEvents={shipmentData?.TransitEvents}
+            />
+          </Col>
+
+          <Col className="mb-[20px] md:mb-0">
+            <Address shipmentData={shipmentData} />
+          </Col>
+        </Row>
+      </Container>
+    </section>
   )
 }
 
--- src/components/StepsProgressBar/StepsProgressBar.jsx
+++ src/components/StepsProgressBar/StepsProgressBar.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+// DELIVERED
+// CANCELLED
+// DELIVERED_TO_SENDER
+
 const StepsProgressBar = () => {
   return (
     <div>StepsProgressBar</div>
